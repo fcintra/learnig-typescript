4864b69f5d42e0d4597808fdaefb48af79a9cb14
fcintra <matheus.cintra_8@hotmail.com>
2022-04-04 17:48:40 -0300

    models

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,17 +1,18 @@
 export class Negociacao
 {
-
     constructor(
-        public readonly data: Date, 
-        private readonly quantidade: number, 
-        private readonly valor: number
-    ){}
-
-
+        private  _data: Date, 
+        public readonly quantidade: number, 
+        public readonly valor: number
+    ){};
     get volume(): number 
     {
         return this.quantidade * this.valor;
     }
-  
+    get data(): Date
+    {
+        const data = new Date(this._data.getTime());
+        return data;
+    }
 
 }
